908616db923d677166c118dc67757f339a849048
Qasim <mq12113@gmail.com>
2024-03-02 22:30:25 +0000

    added new project interactive Maps

--- Day_21_Sticky_Navbar_onScroll/script2.js
+++ Day_21_Sticky_Navbar_onScroll/script2.js
@@ -1,32 +1,27 @@
 const links = document.querySelectorAll(".nav-list li a")
 
-for (link of links){
+for (link of links) {
     link.addEventListener("click", smoothScroll)
-
 }
 
 function smoothScroll(e) {
     e.preventDefault()
 
-    const href = this.getAttribute("href");
+    const href = this.getAttribute("href")
 
     document.querySelector(href).scrollIntoView({
         behavior: "smooth"
-
     })
-    
 }
 
 
 
-// Sticky Header
-
 window.addEventListener("scroll", () => {
     const header = document.querySelector("header");
-
-    header.classList.toggle("sticky", window.scrollY > 0);
+    header.classList.toggle("sticky", window.scrollY > 0)
 
     const logo = document.querySelector(".logo")
+
 
     // if (window.scrollY > 0) {
     //     logo.style.color = "white"
@@ -35,8 +30,6 @@ window.addEventListener("scroll", () => {
 
     // }
 
-    window.scrollY > 0 ? logo.style.color = "white" : logo.style.color = "var(--color-sec)"
-
-    
+    window.scrollY > 0 ? logo.style.color = "white" : logo.style.color = "var(--color-sec)"    
 
 })
---
+++ Day_90_Interactive Map/script.js
@@ -0,0 +1,49 @@
+
+ async function getData(place, countryName) {
+    const url = `https://timezone.abstractapi.com/v1/current_time/?api_key=b601376d3ad448cd944357501f9449ac&location=${place}`
+
+        const response = await fetch(url)
+        const data = await response.json()
+        console.log(await response.json());
+        const time = data.datetime
+
+        // const dateTimeObject = new Date(data.datetime);
+        // if you want to change the time formate
+        // const formattedDateTime = dateTimeObject.toLocaleString();
+        // const formattedDateTime = dateTimeObject.toLocaleTimeString() + " " + dateTimeObject.toLocaleDateString();
+
+        const timeEl = document.getElementById("time")
+        // timeEl.innerText = `${countryName} time = ${formattedDateTime} ${data.timezone_abbreviation}`;
+        timeEl.innerText = `${countryName} time = ${time} ${data.timezone_abbreviation}`;
+        timeEl.style.opacity = 1;
+
+        setTimeout(() => {
+            timeEl.style.opacity = 0;
+        },3000)
+    }
+
+    document.querySelectorAll(".allPaths").forEach(e=> {
+        e.addEventListener("mouseover", function() {
+            window.onmousemove = function(j) {
+                x = j.clientX
+                y = j.clientY
+
+                document.getElementById("name").style.top = y-20+"px"
+                document.getElementById("name").style.left = x+10+"px"
+            }
+
+            e.style.fill = "grey"
+            document.getElementById("name").style.opacity = 1
+            document.getElementById("namep").innerText = e.id
+        })
+
+        e.addEventListener("mouseleave", function() {
+            e.style.fill = "#ececec"
+            document.getElementById("name").style.opacity = 0
+        })
+
+        e.addEventListener("click", function() {
+            getData(e.id, e.getAttribute("name"))
+        })
+    })
+
